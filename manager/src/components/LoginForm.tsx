3f9a40f37aec6da620d62d46c7ed35d4d039ab8b
Kenta Kodashima <kentakodashima@gmail.com>
2018-12-09 17:36:44 -0800

    Change .js files in the components folder to .tsx.
    TODO:
     - Resolve Typescript lint error

---
+++ manager/src/components/LoginForm.tsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { emailChanged } from '../actions'
+import { Card, CardSection, Input, Button } from './common'
+
+class LoginForm extends Component {
+  onEmailChange(text: string) {
+    // Create an action using 'emailChanged' action creator
+    this.props.emailChanged(text)
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input 
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input 
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button 
+            title="Login"
+          />
+        </CardSection>
+      </Card>
+    )
+  }
+}
+
+// 1st : mapStateToProps
+const mapStateToProps = (state: any) => {
+  return {
+    email: state.auth.email
+  }
+}
+
+// 2nd : action creator 
+// Bind 'emailChanged' action creator to this componet
+export default connect(null, { emailChanged })(LoginForm)
